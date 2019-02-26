468be8debcfea101f33e619e2fe68c9014768350
Allan Brito <falecom@allanbrito.com.br>
2019-02-26 06:46:27 -0300

    Cria rota para a SPA

--- app/index.php
+++ app/index.php
@@ -1,3 +1,8 @@
 <?php
 
 require 'bootstrap.php';
+
+use App\Config\Router;
+
+$router = new Router;
+$router->resolve($_GET['url']);
---
+++ app/src/config/Router.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Config;
+
+class Router
+{
+    private $routes = [
+        '' 		    => ['GET' => 'App\\Controllers\\HomeController::index'],
+        '/'		    => ['GET' => 'App\\Controllers\\HomeController::index'],
+    ];
+
+    public function resolve(string $url)
+    {
+        if (empty($this->routes[$url][$_SERVER['REQUEST_METHOD']]))
+        {
+            http_response_code(404);
+            echo '404 - Not Found';
+            return;
+        }
+
+        call_user_func($this->routes[$url][$_SERVER['REQUEST_METHOD']]);
+    }
+}
